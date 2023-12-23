3bd8d88a122c434c952d840d272f5f6258c0a7ff
Piengrawee Phetchuai <piengraweepechchauy@gmail.com>
2023-12-23 15:25:25 +0700

    Save changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DEF81D",
+        primary: "#568F7C",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
         "wd-darkgrey": "#2D2D2D",
